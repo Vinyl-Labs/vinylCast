440d48666b6cbab1067962887127a89a34e90c63
SlayTheSensei <kylejpearson@gmail.com>
2019-04-27 10:36:43 -0400

    Initial commit

---
+++ app/src/main/java/com/checkvinyl/vinylcast/MainActivity.java
@@ -0,0 +1,275 @@
+package com.checkvinyl.vinylcast;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.wowza.gocoder.sdk.api.WowzaGoCoder;
+import com.wowza.gocoder.sdk.api.broadcast.WOWZBroadcast;
+import com.wowza.gocoder.sdk.api.broadcast.WOWZBroadcastConfig;
+import com.wowza.gocoder.sdk.api.configuration.WOWZMediaConfig;
+import com.wowza.gocoder.sdk.api.devices.WOWZAudioDevice;
+import com.wowza.gocoder.sdk.api.devices.WOWZCameraView;
+import com.wowza.gocoder.sdk.api.errors.WOWZError;
+import com.wowza.gocoder.sdk.api.errors.WOWZStreamingError;
+import com.wowza.gocoder.sdk.api.status.WOWZState;
+import com.wowza.gocoder.sdk.api.status.WOWZStatus;
+import com.wowza.gocoder.sdk.api.status.WOWZStatusCallback;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MainActivity extends Activity implements WOWZStatusCallback, View.OnClickListener {
+
+
+    // The top-level GoCoder API interface
+    private WowzaGoCoder goCoder;
+
+
+    // The GoCoder SDK camera view
+    private WOWZCameraView goCoderCameraView;
+
+    // The GoCoder SDK audio device
+    private WOWZAudioDevice goCoderAudioDevice;
+
+    // The GoCoder SDK broadcaster
+    private WOWZBroadcast goCoderBroadcaster;
+
+    // The broadcast configuration settings
+    private WOWZBroadcastConfig goCoderBroadcastConfig;
+
+    // Properties needed for Android 6+ permissions handling
+    private static final int PERMISSIONS_REQUEST_CODE = 0x1;
+    private boolean mPermissionsGranted = true;
+    private String[] mRequiredPermissions = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the GoCoder SDK
+        goCoder = WowzaGoCoder.init(getApplicationContext(), "GOSK-3B46-010C-E7D4-2AE6-593D");
+
+        if (goCoder == null) {
+            // If initialization failed, retrieve the last error and display it
+            WOWZError goCoderInitError = WowzaGoCoder.getLastError();
+            Toast.makeText(this,
+                    "GoCoder SDK error: " + goCoderInitError.getErrorDescription(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        // Associate the WOWZCameraView defined in the U/I layout with the corresponding class member
+        goCoderCameraView = (WOWZCameraView) findViewById(R.id.camera_preview);
+
+        // Create an audio device instance for capturing and broadcasting audio
+        goCoderAudioDevice = new WOWZAudioDevice();
+        goCoderAudioDevice.setAudioSource(MediaRecorder.AudioSource.MIC);
+
+        // Create a broadcaster instance
+        goCoderBroadcaster = new WOWZBroadcast();
+
+        // Create a configuration instance for the broadcaster
+        goCoderBroadcastConfig = new WOWZBroadcastConfig(WOWZMediaConfig.FRAME_SIZE_1920x1080);
+
+        // Set the connection properties for the target Wowza Streaming Engine server or Wowza Streaming Cloud live stream
+        goCoderBroadcastConfig.setHostAddress("192.168.1.151");
+        goCoderBroadcastConfig.setPortNumber(1935);
+        goCoderBroadcastConfig.setApplicationName("vinyl");
+        goCoderBroadcastConfig.setStreamName("lab");
+        goCoderBroadcastConfig.setUsername("vinyladmin");
+        goCoderBroadcastConfig.setPassword("Likewind1");
+        goCoderBroadcastConfig.setVideoEnabled(false);
+
+        goCoderBroadcastConfig.setAudioBitRate(48000);
+
+
+        // Removes video from the broadcast
+//        goCoderBroadcastConfig.setVideoEnabled(false);
+
+        // Designate the camera preview as the video source
+        goCoderBroadcastConfig.setVideoBroadcaster(goCoderCameraView);
+
+        // Designate the audio device as the audio broadcaster
+        goCoderBroadcastConfig.setAudioBroadcaster(goCoderAudioDevice);
+
+        Button broadcastButton = findViewById(R.id.broadcast_button);
+        broadcastButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//         If running on Android 6 (Marshmallow) or above, check to see if the necessary permissions
+//         have been granted
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mPermissionsGranted = hasPermissions(this, mRequiredPermissions);
+            if (!mPermissionsGranted)
+                this.requestPermissions( mRequiredPermissions, PERMISSIONS_REQUEST_CODE);
+        } else
+            mPermissionsGranted = true;
+
+        // Start the camera preview display
+        if (mPermissionsGranted && goCoderCameraView != null) {
+            if (goCoderCameraView.isPreviewPaused())
+                goCoderCameraView.onResume();
+            else
+                goCoderCameraView.startPreview();
+        }
+    }
+
+    //
+    // Callback invoked in response to a call to ActivityCompat.requestPermissions() to interpret
+    // the results of the permissions request
+    //
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        mPermissionsGranted = true;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE: {
+                // Check the result of each permission granted
+                for (int grantResult : grantResults) {
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        mPermissionsGranted = false;
+                    }
+                }
+            }
+        }
+    }
+
+    //
+// Utility method to check the status of a permissions request for an array of permission identifiers
+//
+    private static boolean hasPermissions(Context context, String[] permissions) {
+        for (String permission : permissions)
+            if (context.checkCallingOrSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+
+        return true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        if (rootView != null)
+            rootView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    //
+// The callback invoked upon changes to the state of the broadcast
+//
+    public void onWZStatus(final WOWZStatus goCoderStatus) {
+        // A successful status transition has been reported by the GoCoder SDK
+        final StringBuffer statusMessage = new StringBuffer("Broadcast status: ");
+
+        switch (goCoderStatus.getState()) {
+            case WOWZState.STARTING:
+                statusMessage.append("Broadcast initialization");
+                break;
+
+            case WOWZState.READY:
+                statusMessage.append("Ready to begin streaming");
+                break;
+
+            case WOWZState.RUNNING:
+                statusMessage.append("Streaming is active");
+                break;
+
+            case WOWZState.STOPPING:
+                statusMessage.append("Broadcast shutting down");
+                break;
+
+            case WOWZState.IDLE:
+                statusMessage.append("The broadcast is stopped");
+                break;
+
+            default:
+                return;
+        }
+
+        // Display the status message using the U/I thread
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, statusMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    //
+// The callback invoked when an error occurs during a broadcast
+//
+    public void onWZError(final WOWZStatus goCoderStatus) {
+        // If an error is reported by the GoCoder SDK, display a message
+        // containing the error details using the U/I thread
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this,
+                        "Streaming error: " + goCoderStatus.getLastError().getErrorDescription(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        // return if the user hasn't granted the app the necessary permissions
+        if (!mPermissionsGranted) return;
+
+        // Ensure the minimum set of configuration settings have been specified necessary to
+        // initiate a broadcast streaming session
+        WOWZStreamingError configValidationError = goCoderBroadcastConfig.validateForBroadcast();
+
+        if (configValidationError != null) {
+            Toast.makeText(this, configValidationError.getErrorDescription(), Toast.LENGTH_LONG).show();
+        } else if (goCoderBroadcaster.getStatus().isRunning()) {
+            // Stop the broadcast that is currently running
+            goCoderBroadcaster.endBroadcast(this);
+        } else {
+            // Start streaming
+            goCoderBroadcaster.startBroadcast(goCoderBroadcastConfig, this);
+        }
+    }
+}
+
+
